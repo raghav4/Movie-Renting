2712e45ac8cd31280971c9cbd3c70a54ee8d79e8
Raghav Sharma <raghavsyt@gmail.com>
2019-05-22 23:06:38 +0530

    new update

--- README.md
+++
@@ -1,3 +0,0 @@
-# Movie-Renting-Backend
-
-Use `npm install` to install all the required dependencies. 
--- index.js
+++ index.js
@@ -1,15 +1,58 @@
+const Joi = require('joi');
 const express = require('express');
-const genres = require('./router/genres');
-const customers = require('./router/customers');
-const mongoose = require('mongoose');
 const app = express();
 
-mongoose.connect('mongodb://localhost/MovieRenting')
-    .then(() => console.log('Connected to MongoDB...'))
-    .catch(err => console.error('Trouble Connecting to MongoDb...'));
-app.use(express.json()); // For handling JSON PUT Requests
-app.use('/api/genres', genres); // Any  route with /api/courses should be handled as genres router
-app.use('/api/customers',customers); // Any  route with /api/customers should be handled as customers router
+app.use(express.json()); //bodyparse: post se req.body me data bharne ka kaam iska he. 
 
-app.listen(3000, () => console.log('Listening on Port 3000...'));
-// TODO : GET ✔️, POST ✔️ , INPUT VALIDATION ✔️ , PUT ✔️, DELETE ✔️
+// GET - Getting the list of all genres.
+const genres = [{id:1,name:'Action'},{id:2,name:'Horror'},{id:3,name:'Romance'}];
+app.get('/api/genres',(req,res)=>{
+    res.send(genres);
+});
+// GET - Getting a single genre.
+app.get('/api/genres/:id',(req,res)=>{
+    const genre = genres.find(g=>g.id===parseInt(req.params.id));
+    if(!genre) return res.status(404).send('The Request Genre with the Given ID is not found!');
+    res.send(genre);
+});
+// POST - Creating the genre.
+app.post('/api/genres',(req,res)=>{
+    // Check for the validation before posting.
+    const {error} = validateGenre(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+    const genre = {
+        id: genres.length + 1,
+        name: req.body.name
+    };
+    genres.push(genre);
+    res.send(genre);
+});
+app.put('/api/genres/:id',(req,res)=>{
+    const genre = genres.find(g=>g.id === parseInt(req.params.id));
+    if(!genre){
+        return res.status(404).send("Requested Genre is not Found!!");
+    }
+    const {error} = validateGenre(req.body);
+    if(error)   return res.status(400).send(error.details[0].message);
+    genre.name = req.body.name;
+    res.send(genre);
+});
+app.delete('/api/genres/:id',(req,res)=>{
+    // Search for the given id
+    const genre = genres.find(g=>g.id === parseInt(req.params.id));
+    if(!genre) return res.status(404).send('Requested Genre not found!');
+    const index = genres.indexOf(genre);
+    genres.splice(index,1);
+    res.send(genres);
+});
+const port = process.env.PORT || 3000;
+app.listen(port,()=>console.log(`Listening on port ${port}...`)); // listening on the given port.
+
+function validateGenre(genre){
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+    return Joi.validate(genre,schema);
+}
+
+//TODO : Get : All ✅, Get: Single Genre ✅, Post  ✅
