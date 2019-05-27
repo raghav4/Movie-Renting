c2a6726af4e6465ca091785936eb0523bf7bfeb9
Raghav Sharma <raghavsyt@gmail.com>
2019-05-27 16:46:12 +0530

    + template engine
    pugjs

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const genres = require('./routes/genres');
 
 app.use(express.json()); 
 app.use('/api/genres',genres);
+app.set('view engine', 'pug');
 
 const port = process.env.PORT || 3000;
 app.listen(port,()=>console.log(`Listening on port ${port}...`)); 
