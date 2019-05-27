3b767c3b3435f0880591d3d34465da544724dea6
Raghav Sharma <raghavsyt@gmail.com>
2019-05-27 16:47:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const Joi = require('joi');
 const express = require('express');
 const app = express(); 
 const genres = require('./routes/genres');
+const home = require('./routes/home');
 
 app.use(express.json()); 
 app.use('/api/genres',genres);
+app.use('/',home);
 app.set('view engine', 'pug');
 
 const port = process.env.PORT || 3000;
